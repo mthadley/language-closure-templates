851eda52346023e2fe2d06a4d1e22d60ad304038
Michael Hadley <michael.hadley@liferay.com>
2017-05-21 12:18:57 -0700

    Add tests

---
+++ spec/closure-templates-spec.js
@@ -0,0 +1,20 @@
+'use babel'
+
+import grammarTest from 'atom-grammar-test'
+import path from 'path'
+
+describe('Closure Templates grammar', () => {
+  let grammar = null
+
+  beforeEach(() => {
+    waitsForPromise(() => atom.packages.activatePackage('language-closure-templates'))
+
+    runs(() => grammar = atom.grammars.grammarForScopeName('text.html.soy'))
+  })
+
+  it('should activate grammar', () => {
+    expect(grammar).not.toBeNull()
+  })
+
+  grammarTest(path.join(__dirname, 'fixtures/syntax_test_closure_templates.soy'))
+})
